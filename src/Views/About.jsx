815b134199fb3e09b8a211b8d4f0c770dfc2872f
Metaverse-Nabeel <nabeelahmed3129@gmail.com>
2023-07-23 16:49:59 +0500

    About page text size corrected

--- src/Views/About.jsx
+++ src/Views/About.jsx
@@ -2,11 +2,11 @@ const About = () => {
   return (
     <>
       <div className="flex justify-between items-center h-5/6 w-full mt-32 px-32">
-        <div className="flex flex-col flex-wrap w-48 font-semibold text-7xl">
+        <div className="flex flex-col flex-wrap w-48 font-normal text-8xl">
           who are we?
           <div className="flex py-2 w-24 border-b-8 border-black" />
         </div>
-        <div className="flex flex-wrap py-2 w-2/3 font-normal text-4xl text-left">
+        <div className="flex flex-wrap py-2 w-2/3 font-thin text-6xl text-left">
           We are not just appliance merchants; we are appliance contractors.
           With a team of engineers and drafters, our expertise allows us to
           exclusively serve developers and general contractors with a level of
@@ -15,12 +15,12 @@ const About = () => {
       </div>
       <div className="flex flex-col h-5/6 my-32 px-32">
         <div className="flex flex-col justify-start items-end">
-          <p className="text-left font-semibold w-1/5 text-7xl">
+          <p className="text-left font-normal w-1/5 text-8xl">
             mission and vision
             <div className="py-2 w-24 border-b-8 border-black" />
           </p>
         </div>
-        <div className="flex py-2 w-1/4 font-normal text-4xl text-left">
+        <div className="flex py-2 w-1/2 font-thin text-6xl text-left">
           to provide vertically integrated supply and installation for
           commercial and residential appliances on any project.
         </div>
